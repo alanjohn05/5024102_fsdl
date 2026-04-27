8021b7c8d096929e723ab419aabf44b7dd08fd6d
alanjohn05 <alanjohn632006@gmail.com>
2026-04-27 07:09:05 +0530

    Add a simple HTTP server in Server.py

---
+++ Week09_300326/Server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import http.server, socketserver, os
+PORT = 8000
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+class H(http.server.SimpleHTTPRequestHandler):
+ def end_headers(self):
+ self.send_header('Access-Control-Allow-Origin', '*')
+ super().end_headers()
+ def log_message(self, fmt, *args):
+ print(f" {fmt % args}")
+print(f"\n Experiment 9 running at http://localhost:{PORT}\n Ctrl+C to stop\n")
+with socketserver.TCPServer(("", PORT), H) as s:
+ try: s.serve_forever()
+ except KeyboardInterrupt: print("\n Server stopped.")
